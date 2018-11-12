4d703296b333e505d2c27caf69bcf81f0a733a3d
nickfoden <nickfoden@gmail.com>
2018-11-12 17:07:59 -0500

    Set up stateful component form with typescript for the state

---
+++ src/components/App.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+export class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      currentTask: "",
+      tasks: []
+    };
+  }
+  handleSubmit(e: any) {
+    e.preventDefault();
+    this.setState({
+      currentTask: "",
+      tasks: [...this.state.tasks, this.state.currentTask]
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>React Typescript To Do List </h1>
+
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="Add a task"
+            value={this.state.currentTask}
+            onChange={e => this.setState({ currentTask: e.target.value })}
+          />
+          <button type="submit"> Add Task</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+interface IState {
+  currentTask: string;
+  tasks: Array<string>;
+}
